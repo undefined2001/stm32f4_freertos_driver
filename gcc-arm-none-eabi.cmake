991dbbdf9ee9f45e626a099b99dbd51ced79f1ea
undefined2001 <asraful.islam.taj@gmail.com>
2025-05-31 17:00:23 +0600

    Writing a Windows/Linux Supported CMake file

--- gcc-arm-none-eabi.cmake
+++ gcc-arm-none-eabi.cmake
@@ -1,11 +1,30 @@
-set(TOOLCHAIN_PREFIX arm-none-eabi-)
+set(CMAKE_SYSTEM_NAME Generic)
+set(CMAKE_SYSTEM_PROCESSOR Arm)
 
-set(CMAKE_ASM_COMPILER /usr/bin/arm-none-eabi-gcc)
-set(CMAKE_C_COMPILER /usr/bin/arm-none-eabi-gcc)
-set(CMAKE_CXX_COMPILER /usr/bin/arm-none-eabi-g++)
-set(CMAKE_OBJCOPY ${TOOLCHAIN_PREFIX}objcopy)
-set(CMAKE_OBJDUMP ${TOOLCHAIN_PREFIX}objdump)
-set(CMAKE_SIZE ${TOOLCHAIN_PREFIX}size)
+
+if(CMAKE_HOST_SYSTEM_NAME STREQUAL "Linux")
+
+    message("#################### OS Info ###################")
+    message("#                   Linux                      #")
+    message("################################################")
+
+
+    set(TOOLCHAIN_PATH_PREFIX /usr/bin/arm-none-eabi)
+    set(CMAKE_ASM_COMPILER ${TOOLCHAIN_PATH_PREFIX}-gcc)
+    set(CMAKE_C_COMPILER ${TOOLCHAIN_PATH_PREFIX}-gcc)
+    set(CMAKE_CXX_COMPILER ${TOOLCHAIN_PATH_PREFIX}-g++)
+    set(CMAKE_OBJCOPY ${TOOLCHAIN_PATH_PREFIX}-objcopy)
+    set(CMAKE_OBJDUMP ${TOOLCHAIN_PATH_PREFIX}-objdump)
+    set(CMAKE_SIZE ${TOOLCHAIN_PATH_PREFIX}-size)
+
+elseif(CMAKE_HOST_SYSTEM_NAME STREQUAL "Windows")
+    message("This is a Windows System")
+else()
+    message(ERROR "Unsupported System")
+endif()
+
+
+
 
 set(STARTUP ${CMAKE_SOURCE_DIR}/core/Startup/startup_stm32f446retx.c)
 set(LINKER ${CMAKE_SOURCE_DIR}/STM32F446RETX_FLASH.ld)
